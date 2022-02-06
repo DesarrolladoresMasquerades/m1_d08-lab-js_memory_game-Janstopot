41cb7d88285341ca02eec8b004fbddf2e1874367
Alejandro Martinez <alejandro.mzrz@gmail.com>
2022-02-06 16:42:36 +0100

    memory

--- js/memory.js
+++ js/memory.js
@@ -2,19 +2,57 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.score = 0;
+    this.guessedPair = 0;
+    this.clickedPairs = 0;
+    this.playedCards = [];
+    this.shuffleCards()
   }
 
   shuffleCards() {
-    // ... write your code here
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
   }
 
-  checkIfPair(card1, card2) {
-    // ... write your code here
+  checkIfPair() {
+    console.log(this.playedCards)
+    if(
+      // alternative syntax: this.playedCards[].getAttribute('cata-card-name')
+      this.playedCards[0].dataset.cardName === this.playedCards[1].dataset.cardName 
+    ){
+      return true
+    }
   }
 
-  checkIfFinished() {
-    // ... write your code here
+  checkIfGameOver() {
+    // ------ add antoher logic here -------
+    // ------ if (this.timer > 60 * 1000) return false
+    // ------ if (this.clickedPairs > 5) return true
+    return this.guessedPairs === this.cards.length / 2
   }
+  playCard(card){
+    let playResults = {"isPair": false, "playedCards": [] }
+    if(this.playedCards.length < 2){
+      this.playedCards.push(card)
+    }
+    if(this.playedCards.length === 2){
+      this.clickedPairs += 1
+      if(this.checkIfPair()){
+        playResults = {"isPair": false, "playedCards": this.playedCards}
+        this.score += 1
+        this.guessedPairs += 1
+        this.playedCards = [];
+      }else{
+        playResults = {"isPair": false, "playedCards": this.playedCards}
+        this.playedCards = [];
+      }
+    }
+    return playResults
+  }
+
+
 }
 
 // The following is required for automated testing. Please, ignore it.
